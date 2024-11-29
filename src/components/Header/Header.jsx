9cd7570f68dd01e5cb16c99376e9f89d56d02b00
HamjaMohtadee <48330979+whitehacker969@users.noreply.github.com>
2024-11-29 23:13:15 +0600

    project initialized and tailwind also fake data created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css'
+const Header = () => {
+    return (
+        <div>
+             <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
